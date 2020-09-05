f7933337519d7ab85063e68827f291a3872640f7
kaushikbhat07 <kaushik.bhatt@learner.manipal.edu>
2020-09-05 14:00:06 +0530

    Services

--- src/components/layout/Main.js
+++ src/components/layout/Main.js
@@ -3,6 +3,7 @@ import './Main.css';
 import Quotation from '../content/Quotation'
 import Revenue from '../content/Revenue'
 import Sales from '../content/Sales'
+import Services from '../content/Services'
 
 function Main() {
 	return (
@@ -19,7 +20,17 @@ function Main() {
 					</div>
 					<div className="col-7">
 						<Revenue />
-					</div>					
+						<div className="row">
+							<div className="col-6">
+								<div className="mt-4">
+									<Services />
+								</div>
+							</div>
+							<div className="col-6">
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum officia sapiente necessitatibus autem accusamus, reprehenderit quis aliquam fugiat rerum modi quasi quibusdam voluptatibus, rem recusandae mollitia vero minus corrupti adipisci.
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 
